6e259b1ad279455429ac7fb86b5ee8acdaa41fa4
Kakou Kan (韓 佳孝 GE) <RJ067223@jpn.mds.honda.com>
2025-10-07 17:40:15 +0900

    因为power_input和tester被GUI代替了，暂时删掉（虽然我觉得如果要是需要从文件里设置检测仪的行为那还是得把tester搞回来）

--- energy.py
+++ energy.py
@@ -1,4 +1,21 @@
 class Energy:
-    def __init__(self, Voltage=0.0):
-        self.Voltage = Voltage
-        self.status = None
+    def __init__(self):
+        self._voltage = 0.0
+        self._status = 'POWER_OFF'
+    @property
+    def status(self):
+        return self._status
+    @status.setter
+    def status(self, value):
+        self._status = value
+        if value == 'POWER_ON':
+            self._voltage=12.0
+        if value == 'POWER_OFF':
+            self._voltage=0.0
+    @property
+    def Voltage(self):
+        return self._voltage
+
+    #@Voltage.setter
+    #def Voltage(self, value):
+    #    self._Voltage = value
