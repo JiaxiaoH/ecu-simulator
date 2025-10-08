997facc3678ca751b1135aaf6fd51bd03460caaf
Kakou Kan (韓 佳孝 GE) <RJ067223@jpn.mds.honda.com>
2025-10-08 18:15:13 +0900

    修改了SID message的打印方法

--- main.py
+++ main.py
@@ -21,10 +21,20 @@ class ButtonPush:
     def send(cls, name, request_queue, entry=None, tree=None):
         if entry is not None and entry.get().strip() != "":
             can_entry= [int(x, 16) for x in entry.get().split()]
+            SID=can_entry[0]
+            subfunction=None
+            dataID=None
+            data=None
             while len(can_entry) < 4:
-                can_entry.append(0)
-            req = RequestMessage(SID=can_entry[0], subfunction = can_entry[1], dataID=can_entry[2], data=can_entry[3])  # 诊断会话请求
-            print(f"[Tester] Sending request to ECU, SID=0x{req.SID:02X}, subfunction=0x{req.subfunction:02X}, subfunction=0x{req.dataID:02X}, data=0x{req.data:02X}")
+                can_entry.append(None)
+            if can_entry[1] is not None:
+                subfunction = can_entry[1]
+            if can_entry[2] is not None:
+                dataID = can_entry[2]
+            if can_entry[3] is not None:
+                data = can_entry[3]
+            req = RequestMessage(SID=SID, subfunction=subfunction, dataID=dataID, data=data)
+            #print(f"[Tester] Sending request to ECU, SID=0x{req.SID:02X}, subfunction=0x{req.subfunction:02X}, subfunction=0x{req.dataID:02X}, data=0x{req.data:02X}")
             request_queue.put(req)
             if tree is not None:
                 req.log_to_treeview(tree)
@@ -62,7 +72,7 @@ def main():
     response_queue = Queue()
     ecu = ECU(energy=energy, request_queue=request_queue, response_queue=response_queue, tree=tree)
     
-    battery_button = tk.Button(root, text="ON/OFF", command=lambda: ButtonPush.battery("POWER", ecu, energy, battery_button))
+    battery_button = tk.Button(root, text="POWER", command=lambda: ButtonPush.battery("POWER", ecu, energy, battery_button))
     battery_button.place(relx=0.1, rely=0.1, anchor='w')  
 
     entry = tk.Entry(root, width=50)
