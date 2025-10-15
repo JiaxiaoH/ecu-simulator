9500958f8831030ee464dce258084a32663a4af7
JiaxiaoH <RJ067223@jpn.mds.honda.com>
2025-10-15 12:17:32 +0900

    加入了SID

--- ecu.py
+++ ecu.py
@@ -3,11 +3,12 @@ import threading
 import time
 import can
 from SID_0x10 import SID_0x10
+from SID_0x14 import SID_0x14
 from sessiontypes import SESSIONS
 from uds_utils import handle_request_with_timeout
-from can import Notifier
+import random
 class ECU(can.Listener):
-    def __init__(self, energy, bus: can.Bus, tree=None, frequency_hz=60):
+    def __init__(self, energy, bus: can.Bus, frequency_hz=60):
         self.energy = energy
         self.DiagnosticSession = SESSIONS.DEFAULT_SESSION
         self.arbitration_id = 0x002
@@ -18,12 +19,13 @@ class ECU(can.Listener):
         self.running = False
         self.start_event = threading.Event()
         self.stop_event = threading.Event()
-        #self.timer_lock = threading.Lock()
-        #self.timer = 0
         self.lock = threading.Lock()
         self.security=False
-        self.tree = tree
         self.received_request=False
+        self.moving=False
+        self.dtc=None
+        self.is_memory_error=False
+
     def on_power_status_changed(self, status):
         if status == "POWER_ON":
             print("[ECU] Received POWER_ON signal, starting ECU...")
@@ -46,7 +48,8 @@ class ECU(can.Listener):
                 request=msg,
                 handler_func=self.handle_request,
                 timeout=self.P2_time,
-                on_timeout=self.on_timeout
+                on_timeout=self.on_timeout,
+                on_finish=self.on_finish
             )
             if response:
                 self.bus.send(response)
@@ -61,26 +64,13 @@ class ECU(can.Listener):
         while not self.stop_event.is_set():
             if self.running:
                 start_time = time.time()
-                # 这里放置ECU的循环处理逻辑
-                #尝试通信
-                # 收到请求，启动处理逻辑 -> 移动到on_message_received
-                # 超时无请求则继续循环
-                # 频率控制（60Hz，周期1/60秒），
-                # 如果当前没有处理request，就要sleep保证周期
                 if not self.received_request:
-                    #self.loop_task()
                     elapsed = time.time() - start_time
                     sleep_time = self.period - elapsed
                     if sleep_time > 0:
                         time.sleep(sleep_time)
 
         print("[ECU] Stop event set, exiting...")
-
-    # def loop_task(self):
-    #     # 模拟ECU每个周期要做的任务
-    #     with self.timer_lock:
-    #         self.timer += 1
-    #         #print(f"[{self.timer}] ECU loop running at {time.time():.4f}")
 
     def start(self):
         self.start_event.set()
@@ -97,10 +87,40 @@ class ECU(can.Listener):
         except can.CanError as e:
             print(f"[ERROR] CAN send failed (timeout response): {e}")
     
+    def on_finish(self, resp):
+        if resp is not None:
+            try:
+                self.bus.send(resp)
+                print(f"[ECU] Sent response after extended processing: {resp}")
+            except can.CanError as e:
+                print(f"[ERROR] CAN send failed (extende processing)")
+
     def handle_request(self, req):
         try:
             if req.data[0]==0x10:#SID
                 resp=SID_0x10.handle(req, self) 
                 return resp
+            if req.data[0]==0x14:
+                resp=SID_0x14.handle(req,self)
+                return resp
         except Exception as e:
             print(f"[ECU] Error processing message: {e}")
+    
+    def dtc_clear(self):
+        try:
+            self.timeout_set(self.random_set())
+            self.dtc=None
+            print("Success: DTC reset!")
+        except Exception as e:
+            self.is_memory_error=True
+            print("Failed: DTC reset!")
+    
+    def random_set(self, rn=0.2):
+        if random.random() < rn:  
+            return True
+        else:
+            return False
+    
+    def timeout_set(self, flag):
+        if flag:
+            time.sleep(self.P2_time+1)
